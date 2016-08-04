79a73c388b0298b1d89782df0b8d883881ca502a
Daniel Christo <daniel.christo@jonahgroup.com>
2016-08-04 18:54:52 -0500

    Added race totals
    Added total km, total time running etc to the footer of the running table.

--- websitejs/angularHomePageApp.js
+++ websitejs/angularHomePageApp.js
@@ -13,26 +13,45 @@ angular.module('homePageApp', [])
     ];
 
     this.races = [
-      {date:'2016/06/25', race:'Perth Kilt Run', km:'21', time:'2:34:50.3'},
-      {date:'2016/05/08', race:'Sporting Life 10k', km:'10', time:'0:49:30'},
-      {date:'2016/04/03',	race:'Around the Bay Road Race', km:'30', time:'3:16:59'},
-      {date:'2014/08/16',	race:'A Mid Summer Nights Run', km:'30', time:'2:56:42.2'},
-      {date:'2013/10/20',	race:'Toronto Waterfront Marathon', km:'42.2', time:'4:50:02'},
-      {date:'2014/05/11',	race:'Sporting Life 10k', km:'10', time:'0:51:16.7'},
-      {date:'2013/09/11',	race:'Mount Robson Marathon', km:'42.2', time:'6:22:53.7'},
-      {date:'2013/08/17',	race:'A Mid Summer Nights Run', km:'30', time:'3:03:59.1'},
-      {date:'2013/04/21',	race:'Toronto Yonge Street 10k', km:'10', time:'0:49:34'},
-      {date:'2013/05/12',	race:'Sporting Life 10k', km:'10', time:'0:48:24'},
-      {date:'2012/10/14',	race:'Toronto Waterfront Marathon', km:'42.2', time:'4:36:39.0'},
-      {date:'2012/08/18',	race:'A Mid Summer Nights Run', km:'30', time:'3:04:24.4'},
-      {date:'2012/03/25',	race:'Around the Bay Road Race', km:'30', time:'3:18:01.1'},
-      {date:'2011/10/01',	race:'Run for the Toad', km:'25', time:'3:18:44'},
-      {date:'2011/09/24',	race:'ZooRun 10k', km:'10', time:'0:56:47.6'},
-      {date:'2011/05/01',	race:'Sporting Life Toronto 10k', km:'10', time:'0:58:49.7'},
-      {date:'2011/03/27',	race:'Around the Bay Road Races', km:'30', time:'3:24:06.5'},
-      {date:'2010/09/26',	race:'Toronto Waterfront Marathon', km:'42.2', time:'	5:05:22.3'},
-      {date:'2010/08/21',	race:'A Mid Summer Nights Run', km:'30', time:'3:13:30.4'}
+      {date:'2016-06-25', race:'Perth Kilt Run', km:'21', time:'2:34:50.3'},
+      {date:'2016-05-08', race:'Sporting Life 10k', km:'10', time:'0:49:30'},
+      {date:'2016-04-03',	race:'Around the Bay Road Race', km:'30', time:'3:16:59'},
+      {date:'2014-08-16',	race:'A Mid Summer Nights Run', km:'30', time:'2:56:42.2'},
+      {date:'2013-10-20',	race:'Toronto Waterfront Marathon', km:'42.2', time:'4:50:02'},
+      {date:'2014-05-11',	race:'Sporting Life 10k', km:'10', time:'0:51:16.7'},
+      {date:'2013-09-11',	race:'Mount Robson Marathon', km:'42.2', time:'6:22:53.7'},
+      {date:'2013-08-17',	race:'A Mid Summer Nights Run', km:'30', time:'3:03:59.1'},
+      {date:'2013-04-21',	race:'Toronto Yonge Street 10k', km:'10', time:'0:49:34'},
+      {date:'2013-05-12',	race:'Sporting Life 10k', km:'10', time:'0:48:24'},
+      {date:'2012-10-14',	race:'Toronto Waterfront Marathon', km:'42.2', time:'4:36:39.0'},
+      {date:'2012-08-18',	race:'A Mid Summer Nights Run', km:'30', time:'3:04:24.4'},
+      {date:'2012-03-25',	race:'Around the Bay Road Race', km:'30', time:'3:18:01.1'},
+      {date:'2011-10-01',	race:'Run for the Toad', km:'25', time:'3:18:44'},
+      {date:'2011-09-24',	race:'ZooRun 10k', km:'10', time:'0:56:47.6'},
+      {date:'2011-05-01',	race:'Sporting Life Toronto 10k', km:'10', time:'0:58:49.7'},
+      {date:'2011-03-27',	race:'Around the Bay Road Races', km:'30', time:'3:24:06.5'},
+      {date:'2010-09-26',	race:'Toronto Waterfront Marathon', km:'42.2', time:'	5:05:22.3'},
+      {date:'2010-08-21',	race:'A Mid Summer Nights Run', km:'30', time:'3:13:30.4'}
     ];
+
+    //start race totals
+    this.totalRaces = this.races.length;
+    var totalKm = 0;
+    var totalTime = 0;
+    for (var i = 0; i < this.races.length; i++) {
+      totalKm = totalKm + parseFloat(this.races[i].km);
+      var timeParts = this.races[i].time.split(':');
+      totalTime = totalTime + (parseFloat(timeParts[0]) * 60 * 60) + parseFloat(timeParts[1]) * 60 + parseFloat(timeParts[2]);
+    }
+    this.totalKm = totalKm.toFixed(1);
+    this.totalSeconds = Math.round(totalTime % 60);
+    this.totalMinutes = Math.round((totalTime % (60 * 60)) / 60);
+    this.totalHours = Math.floor(totalTime / (60 * 60));
+    var firstDate = new Date(this.races[0].date);
+    var lastDate = new Date(this.races[this.races.length-1].date);
+    this.totalTimeRunningYears = Math.floor((firstDate - lastDate) / (60 * 60 * 24 * 1000 * 365.25));
+    this.totalTimeRunningDays = Math.round(((firstDate - lastDate) / (60 * 60 * 24 * 1000)) % 365.25);
+    // end race totals
 
     this.skills = [
       { headingTitle: 'Database Programmer', subHeadings:
